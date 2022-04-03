7e2be249d097de2fb7877c64caa50e178298fd07
Matt <mbrowne108@gmail.com>
2022-04-03 01:49:53 -0400

    Finish new recipe render error, add unique ids to each dynamic field

--- client/src/Components/Recipes/NewRecipeForm.js
+++ client/src/Components/Recipes/NewRecipeForm.js
@@ -3,9 +3,8 @@ import React, { useState } from 'react';
 function NewRecipeForm({ ingredients, onNewRecipe }) {
     const [errors, setErrors] = useState([])
     const [ingredientFields, setIngredientFields] = useState([
-        {measurements: '', ing_id: ''}
+        {measurements: '', ing_id: '', id: Math.floor(Math.random() * 10000)}
     ])
-
 
     const [formData, setFormData] = useState({
         name: '',
@@ -15,25 +14,29 @@ function NewRecipeForm({ ingredients, onNewRecipe }) {
     })
 
     function addIngredientFields() {
-        const newField = {measurements: '', ing_id: ''}
+        const newField = {measurements: '', ing_id: '', id: Math.floor(Math.random() * 10000)}
         setIngredientFields([...ingredientFields, newField])
     }
 
-    // function removeIngredientFields(index) {
-    //     let data = [...ingredientFields]
-    //     data.splice(index, 1)
-    //     setIngredientFields(data)
-    // }
+    function removeIngredientFields(index) {
+        let data = [...ingredientFields]
+        data.splice(index, 1)
+        setIngredientFields(data)
+
+        formData.measurements.splice(index, 1)
+        formData.ingredient_ids.splice(index, 1)
+    }
 
     function handleChange(e, index) {
         let value = e.target.value
 
-        if (e.target.name === "ingredient_ids" || e.target.name === "button") {
+        if (e.target.name === "ingredient_ids") {
             const options = e.target.options
+            console.log(options)
             value = []
             ingredientFields[index].ing_id = e.target.value
             for (let i = 0, l = ingredientFields.length; i < l; i++) {
-                for (var oi = 0, ol = options.length; oi < ol; oi++) {
+                for (let oi = 0, ol = options.length; oi < ol; oi++) {
                     if (options[oi].selected) {
                         value.push(ingredientFields[i].ing_id)
                     }
@@ -70,7 +73,7 @@ function NewRecipeForm({ ingredients, onNewRecipe }) {
                     ingredients: [],
                     measurements: []
                 })
-                setIngredientFields([{measurements: '', ing_id: ''}])
+                setIngredientFields([{measurements: '', ing_id: '', id: Math.floor(Math.random() * 10000)}])
             } else {
                 r.json().then(err => setErrors(err.errors))
             }
@@ -97,31 +100,27 @@ function NewRecipeForm({ ingredients, onNewRecipe }) {
                 </div>
                 {ingredientFields.map((input, index) => {
                     return (
-                        <div className='row' key={index}>
+                        <div className='row' key={input.id}>
                             <div className='col-sm-4'>
                                 <input 
                                     className='form-control' 
                                     name="measurements" 
                                     placeholder="Measure" 
                                     value={input.measurements} 
-                                    onChange={e => handleChange(e,index)}
+                                    onChange={e => handleChange(e, index)}
                                 />
                             </div>
                             <div className='col-sm-6'>
-                                <select className='form-select' name="ingredient_ids" onChange={e => handleChange(e,index)}>
+                                <select className='form-select' name="ingredient_ids" onChange={e => handleChange(e, index)}>
                                     <option value=''>Choose Ingredient</option>
                                     {ingredients.map((ingredient) => {
                                         return <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
                                     })}
                                 </select>
                             </div>
-                            {/* <div className='col-sm-1'>
-                                <button 
-                                    className="btn-outline-secondary btn-sm" 
-                                    type="button" 
-                                    onClick={() => removeIngredientFields(index,input)}
-                                >➖</button>
-                            </div> */}
+                            <div className='col-sm-1'>
+                                <button type="button" className="btn-outline-secondary btn-sm" onClick={() => removeIngredientFields(index)}>➖</button>
+                            </div>
                         </div>
                     )
                 })}
