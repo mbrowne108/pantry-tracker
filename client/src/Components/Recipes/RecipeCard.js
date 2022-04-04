10dcb9290845f9e14dd9af1b1da6383404ce8f8c
Matt <mbrowne108@gmail.com>
2022-04-04 10:51:01 -0400

    Add back shopping cart buttons

--- client/src/Components/Recipes/RecipeCard.js
+++ client/src/Components/Recipes/RecipeCard.js
@@ -6,6 +6,22 @@ function RecipeCard({ recipe, itemColor, onUpdateIngredient, onDeleteRecipe }) {
     const measurements = recipe.measurements.replace('[','').replace(']','').replaceAll('"', '').split(', ')
     const ingredients = recipe.ingredients.map((ing,i) => Object.assign(ing, {measurement: measurements[i]}))
 
+    function handleUpdate(e) {
+        const ingredient = ingredients.find(ing => ing.id == e.target.value)
+
+        fetch(`/ingredients/${ingredient.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                in_shopping_list: !ingredient.in_shopping_list 
+            }),
+        })
+            .then(r => r.json())
+            .then((updatedIngredient) => onUpdateIngredient(updatedIngredient))
+    } 
+
     function handleDelete() {
         const result = window.confirm(`Are you sure you want to delete ${recipe.name}?`)
         if (result) {
@@ -31,11 +47,15 @@ function RecipeCard({ recipe, itemColor, onUpdateIngredient, onDeleteRecipe }) {
             <div id={`recipe-details-${recipe.id}`} className='card-body accordion-collapse collapse hide'>
                 <h5>Ingredients:</h5>
                 <div className='list-group list-group-horizontal'>
-                    <ul className={`list-group-item col-6`}>
+                    <ul className={`list-group-item col-8`}>
                         {ingredients.map((ingredient) => {
                             return (
                                 <div className='row' key={ingredient.id}>
-                                    <h6 className={`list-group-item-${itemColor(ingredient)} p-2`}>{ingredient.measurement} — {ingredient.name}</h6> 
+                                    <h6 className={`list-group-item-${itemColor(ingredient)} p-2 col-9`}>{ingredient.measurement} — {ingredient.name}</h6>
+                                    {!ingredient.in_shopping_list ? 
+                                        <button className='btn btn-primary active btn-sm col-3 fa fa-cart-plus' value={ingredient.id} onClick={handleUpdate}></button> :
+                                        <button className='btn btn-secondary btn-sm col-3 fa fa-cart-plus' value={ingredient.id} onClick={handleUpdate}></button>
+                                    }
                                 </div>
                             )
                         })}
